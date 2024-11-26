8869b8860d504ba99f7cfadbcd27e50e8b196f43
GTheD01 <popeftimovgeorgi@yahoo.com>
2024-11-26 18:20:44 +0100

    Upgraded io jwt versions and replaced deprecated methods

--- src/main/java/com/popeftimov/automechanic/global/ErrorResponse.java
+++ src/main/java/com/popeftimov/automechanic/global/ErrorResponse.java
@@ -1,17 +1,14 @@
 package com.popeftimov.automechanic.global;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ErrorResponse {
     private String message;
 
     public ErrorResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
