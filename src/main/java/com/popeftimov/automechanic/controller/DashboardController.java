fe13294cb763be8488438d9354859ced36a03dec
GTheD01 <popeftimovgeorgi@yahoo.com>
2025-04-05 14:47:41 +0200

    Enhance API documentation for DashboardController endpoints

--- src/main/java/com/popeftimov/automechanic/controller/DashboardController.java
+++ src/main/java/com/popeftimov/automechanic/controller/DashboardController.java
@@ -2,7 +2,13 @@ package com.popeftimov.automechanic.controller;
 
 import com.popeftimov.automechanic.dto.AdminDashboardDataDTO;
 import com.popeftimov.automechanic.dto.UserDashboardDataDTO;
+import com.popeftimov.automechanic.exception.ApiError;
 import com.popeftimov.automechanic.service.DashboardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +24,19 @@ public class DashboardController {
     @Autowired
     private final DashboardService dashboardService;
 
+    @Operation(summary = "Get admin dashboard data",
+            description = "Returns the upcoming appointment, total appointments, total cars and total reports"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "401",
+                    description = "Unauthorized - Full Authentication required.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @GetMapping("/admin/dashboard")
     public ResponseEntity<AdminDashboardDataDTO> getAdminDashboardData() {
         AdminDashboardDataDTO adminDashboardData = dashboardService.getAdminDashboardData();
@@ -25,6 +44,19 @@ public class DashboardController {
         return ResponseEntity.ok(adminDashboardData);
     }
 
+    @Operation(summary = "Get logged user dashboard data",
+            description = "Returns the user's upcoming appointment, total user appointments, total user cars, and total user reports"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "401",
+                    description = "Unauthorized - Full Authentication required.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @GetMapping("/dashboard")
     public ResponseEntity<UserDashboardDataDTO> getUserDashboardData() {
         UserDashboardDataDTO userDashboardData = dashboardService.getUserDashboardData();
